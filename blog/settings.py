f89ca46e5eea2e6222113da93d86a25ce7d3f66f
idridjan <frosent95@ya.ru>
2023-07-27 22:49:35 +0300

    0.3

--- blog/settings.py
+++ blog/settings.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 """
 Django settings for blog project.
 
@@ -10,6 +12,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-$x)^yntvg6q*4h(x+i(&+ecf%&73z*92-kh0ds*x*3^!h)nx@s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,9 +120,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
+
+STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, "blog/templates")
+]
+
+STATICFILES_FINDERS = [
+    "django.contrib.static.finders.FileSystemFinder"
+    "django.contrib.static.finders.AppDirectoriesFinder"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
