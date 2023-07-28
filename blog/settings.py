d1198aeaeba41a05e42a99431d4564b0cb16ff11
idridjan <frosent95@ya.ru>
2023-07-28 12:38:07 +0300

    Добавлено лого, подключены статики

--- blog/settings.py
+++ blog/settings.py
@@ -107,11 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
--- main/views.py
+++ main/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница',
+        'values': ['Some', 'Hello', '123']
+    }
+    return render(request, 'main/index.html', {'title': 'Главная страница'})
 
 def about(request):
     return render(request, 'main/about.html')
