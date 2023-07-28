283a901ebc6bbc7c444b38f7d73d8c7ba91b8528
idridjan <frosent95@ya.ru>
2023-07-28 21:01:12 +0300

    0.4

--- main/views.py
+++ main/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 def index(request):
     data = {
         'title': 'Главная страница',
-        'values': ['Some', 'Hello', '123']
     }
     return render(request, 'main/index.html', data)
 
--- news/views.py
+++ news/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    data = {
+        'title': 'Главная страница',
+    }
+    return render(request, 'main/index.html', data)
+
+def about(request):
+    return render(request, 'main/about.html')
