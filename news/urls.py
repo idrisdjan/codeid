96abbbbe54b6462d27fe44ef22d0f9c04fc4a154
idridjan <frosent95@ya.ru>
2023-08-05 12:10:44 +0300

    ???

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.news_home, name='news_home'),
     path('create', views.create, name='create'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news-detail')
 ]
--- news/views.py
+++ news/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView
 
 def news_home(request):
     news = Articles.objects.order_by('-date')[:5]
     return render(request, 'news/news_home.html', {'news': news})
 
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html',
+    context_object_name = 'article'
+
 def create(request):
+    error = ''
     if request.method == 'POST':
         form = ArticlesForm(request.POST)
         if form.is_valid():
