54b2f7e0330a76733775265314e1b7f5ff989867
idridjan <frosent95@ya.ru>
2023-08-06 02:00:24 +0300

    Добавлены:     -комментарии     -лайки     -удаление постов

--- news/models.py
+++ news/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     description = models.TextField('Текст записи')
     author = models.CharField('Имя автора', max_length=100)
     date = models.DateField('Дата публикации')
+    img = models.ImageField('Изображение', upload_to='image/%Y')
 
     def __str__(self):
         return f'{self.title}, {self.author}'
@@ -13,3 +14,23 @@ class Post(models.Model):
     class Meta:
         verbose_name = 'Запись'
         verbose_name_plural = 'Записи'
+        
+class Comments(models.Model):
+    '''Комментарий'''
+    email = models.EmailField()
+    name = models.CharField('Имя', max_length=50)
+    text_comments = models.TextField('Текст комментария', max_length=2000)
+    post = models.ForeignKey(Post, verbose_name='Публикация', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.name}, {self.post}'
+    
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        
+        
+class Likes(models.Model):
+    '''Лайки'''
+    ip = models.CharField('IP-адрес',max_length=100)
+    pos = models.ForeignKey(Post, verbose_name='Публикация', on_delete=models.CASCADE)
