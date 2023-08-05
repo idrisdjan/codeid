ec33333f073fca9f1d2b5f7047e10b6a8993ec61
idridjan <frosent95@ya.ru>
2023-08-05 16:36:40 +0300

    Добавлено:     -админка     -добавление постов

--- news/models.py
+++ news/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    '''Данные о посте'''
+    title = models.CharField('Заголовок записи', max_length=100)
+    description = models.TextField('Текст записи')
+    author = models.CharField('Имя автора', max_length=100)
+    date = models.DateField('Дата публикации')
+
+    def __str__(self):
+        return f'{self.title}, {self.author}'
+    
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
