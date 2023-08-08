d4d23b756697d9b91af669e3c53fd196f6d8cd1b
idridjan <frosent95@ya.ru>
2023-08-08 23:09:33 +0300

    0.0.1

--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostView.as_view()),
+    path('', views.PostView.as_view(), name='home'),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('review/<int:pk>/', views.AddComments.as_view(), name='add_comments'),
     path('<int:pk>/add_likes/', views.AddLike.as_view(), name='add_likes'),
