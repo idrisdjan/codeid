7745abfe18abc4adfc70a7f97cdbb36fee545258
idridjan <frosent95@ya.ru>
2023-08-09 22:00:24 +0300

    0.0.1

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
